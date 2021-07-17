ca046d0bde984375b28b03c51f83b1486bc479ba
xeasonx <azusa_go@hotmail.com>
2021-07-17 13:40:01 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # translate-ext
+
+An experimental firefox extension which translates selected text on a web page from English to Chinese and vice-versa.  
+The translation is done by simply request an URI which is analyzed from Bing translation APP, it only supports word or simple phrase level translation.  
+To load this extension, open firefox, type and goto `about:debugging#/runtime/this-firefox` in omnibox, click "Load Temporary Add-on", then select any file in this extension's directory.  
+To use this extension, select a word on a web page, right click mouse to open context menu, click "Translate me", a notification box will appear on the right bottom of the screen.
---
+++ background.js
@@ -0,0 +1,99 @@
+function onCreated() {
+    if (browser.runtime.lastError) {
+        console.log(`Error: ${browser.runtime.lastError}`);
+    } else {
+        console.log("Item created successfully");
+    }
+}
+
+function translateNotification(title, message) {
+    console.log(message);
+    browser.notifications.create("notify-translate", {
+        "type": "basic",
+        "title": title,
+        "message": message,
+    });
+}
+
+function translatePopup() {
+    browser.browserAction.openPopup();
+}
+
+function translate(word) {
+    let url = `https://cn.bing.com/api/v6/dictionarywords/search?q=${word}&appid=371E7B2AF0F9B84EC491D731DF90A55719C7D209&mkt=zh-cn&pname=bingdict`;
+    console.log(url);
+    fetch(
+        url, 
+        {method: "GET"}
+    ).then(response => {
+        return response.json()
+    }).then(data => {
+        // console.log(data);
+        let results = resolveTranslation(data);
+        let pronunciation = results.pop();
+        if (results.length > 0) {
+            // console.log(results);
+            let items = new Array();
+            results.forEach(item => {
+                items.push([item[0] + ": " + item[1].join(", ")]);
+            });
+            translateNotification(word + ` /${pronunciation}/`, items.join("\n"));            
+        } else {
+            translateNotification(word, "not found")
+        }
+    });
+}
+
+function resolveTranslation(respJson) {
+    let simpleMeaningGroup = new Array();
+    let meaningGroups = respJson["value"][0]["meaningGroups"];
+    // console.log(meaningGroups);
+    for (let i=0; i<meaningGroups.length; i++) {
+        let meaning = meaningGroups[i];
+        if (meaning["partsOfSpeech"][0]["description"] === "快速释义") {
+            let t = meaning["partsOfSpeech"][0]["name"];
+            let results = new Array();
+            meaning["meanings"][0]["richDefinitions"][0]["fragments"].forEach(result => {
+                results.push(result.text);
+            });
+            simpleMeaningGroup.push([t, results]);
+            // console.log(results);
+        } else {
+            break;
+        }
+    };
+    let pronunciation = respJson["value"][0]["pronunciation"];
+    simpleMeaningGroup.push(pronunciation);
+    return simpleMeaningGroup;
+}
+
+browser.contextMenus.create({
+    id: "ctx-menu-translate-me",
+    title: browser.i18n.getMessage("ctxMenuTranlateMe"),
+    contexts: ["selection"]
+}, onCreated);
+
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    console.log(info);
+    console.log(tab);
+    switch (info.menuItemId) {
+        case "ctx-menu-translate-me":
+            console.log(info.selectionText);
+            translate(info.selectionText.trim());
+            break;
+        default:
+            break;
+    }
+});
+
+browser.notifications.onClicked.addListener((ntfId, btnIdx) => {
+    switch (ntfId) {
+        case "notify-translate":
+            console.log("close button clicked")
+            browser.notifications.clear("notify-translate");
+            break;
+        default:
+            break;
+    }
+});
