695294cd2363ce6884fb904764edcffa84f7b867
eason <zi.li@longjingtech.com>
2021-09-29 14:23:50 +0800

    update readme, make sticky note movable

--- README.md
+++ README.md
@@ -2,5 +2,24 @@
 
 An experimental firefox extension which translates selected text on a web page from English to Chinese and vice-versa.  
 The translation is done by simply request an URI which is analyzed from Bing translation APP, it only supports word or simple phrase level translation.  
-To load this extension, open firefox, type and goto `about:debugging#/runtime/this-firefox` in omnibox, click "Load Temporary Add-on", then select any file in this extension's directory.  
-To use this extension, select a word on a web page, right click mouse to open context menu, click "Translate me", a notification box will appear on the right bottom of the screen.
+
+## Usage
+
+### Load extension temporarily
+
+To load this extension, open firefox, type and goto `about:debugging#/runtime/this-firefox` in omnibox, click "Load Temporary Add-on", then select any file in this extension's directory.
+
+### Translate selected text
+
+To use this extension, select a text(word or short phrases) on a web page, right click to open context menu the choose "Translate me", a sticky note with translation result will be shown.  
+You can move the note by dragging it. Click the 'x' button on the left top of the note to close it.
+
+## TODO
+
+- [ ] Add configurations
+  - [ ] history
+    - [ ] `option` permanent
+    - [ ] `spinner` count, max count
+- [ ] Find better translation API
+- [ ] Keep sticky note size fixed
+- [ ] Scrollable content
--- content-script.js
+++ content-script.js
@@ -1,38 +1,53 @@
 let selectedNode;
 let selectedRect;
-let stickyId;
 let clientPort = browser.runtime.connect({name: "client_port"});
-console.log(clientPort);
+let isHold = false;
+let sticky;
+let isStickyExists = false;
+let stickyPosition = [];
+let mousePosition = [];
+let dx;
+let dy;
 
+// console.log(clientPort);
+
+/**
+ * Event listener function for tracking text selection.
+ */
 function onSelected() {
     let selection = window.getSelection();
     if (selection.toString().trim()) {
-        console.log(selection);
+        // console.log(selection);
         selectedNode = selection.anchorNode;
         selectedRect = selection.getRangeAt(0).getBoundingClientRect();
     }   
 }
 
+/**
+ * Create an sticky note element.
+ * This element is build up by 4 divs, the outer most is a container, inside the container there are 2 divs,
+ * one for holding 'action buttons', it now comes with a div which acts a 'close button'. The other for 
+ * holding text contents.
+ * @param {*} x left start point of the sticky note element
+ * @param {*} y top start point of the sticky note element
+ * @param {*} content text content
+ * @returns the sticky note element
+ */
 function createSticky(x, y, content) {
     let d = new Date();
-    let idPrefix = d.getTime();
-    let contentElement = "";
+    let idSuffix = d.getTime();
     let sticky = document.createElement("div");
     let actionBar = document.createElement("div");
     let closeButton = document.createElement("div");
     let stickyNotes = document.createElement("div");
 
-    content.forEach(i => {
-        contentElement += `<p>${i}</p>`;
-    });
-
-    sticky.setAttribute("id", `ext-sticky-${idPrefix}`);
-    stickyNotes.setAttribute("id", `ext-sticky-notes-${idPrefix}`);
-    actionBar.setAttribute("id", `ext-action-${idPrefix}`);
-    closeButton.setAttribute("id", `ext-action-close-${idPrefix}`);
+    sticky.setAttribute("id", `ext-sticky-${idSuffix}`);
+    stickyNotes.setAttribute("id", `ext-sticky-notes-${idSuffix}`);
+    actionBar.setAttribute("id", `ext-action-${idSuffix}`);
+    closeButton.setAttribute("id", `ext-action-close-${idSuffix}`);
 
     closeButton.innerHTML = "x";
-    stickyNotes.innerHTML = contentElement;
+    stickyNotes.innerHTML = content;
 
     sticky.style.position = "fixed";
     sticky.style.left = `${x}px`;
@@ -41,6 +56,15 @@ function createSticky(x, y, content) {
     sticky.style.opacity = "0.8";
     sticky.style.backgroundColor = "#eeee00";
     sticky.style.border = "thin dotted black";
+    sticky.style.font = "normal normal small arial,sans-serif";
+    sticky.style.color = "black";
+    sticky.style.textAlign = "left";
+
+    actionBar.style.borderBottom = "thin dotted black";
+    closeButton.style.width = "20px";
+    closeButton.style.height = "20px";
+    closeButton.style.border = "thin dotted black";
+    closeButton.style.textAlign = "center";
 
     actionBar.appendChild(closeButton);
     sticky.appendChild(actionBar);
@@ -48,28 +72,88 @@ function createSticky(x, y, content) {
 
     closeButton.addEventListener("click", evt => {
         sticky.remove();
+        isStickyExists = false;
     });
 
-    return [idPrefix, sticky];
+    closeButton.addEventListener("mouseover", (evt) => {
+        closeButton.style.fontWeight = "bold";
+    });
+
+    closeButton.addEventListener("mouseleave", (evt) => {
+        closeButton.style.fontWeight = "normal";
+    });
+
+    actionBar.addEventListener("mouseleave", (evt) => {
+        sticky.style.cursor = "default";
+    });   
+
+    sticky.addEventListener("mouseover", (evt) => {
+        sticky.style.opacity = "1.0";
+    });
+
+    sticky.addEventListener("mouseleave", (evt) => {
+        sticky.style.opacity = "0.8";
+        isHold = false;
+        sticky.style.cursor = "default";
+    });
+
+    sticky.addEventListener("mousedown", (ev) => {
+        mousePosition = [ev.clientX, ev.clientY];
+        isHold = true;
+        dx = mousePosition[0] - parseInt(sticky.style.left);
+        dy = mousePosition[1] - parseInt(sticky.style.top);
+        // console.log(dx, dy);
+        // console.log(mousePosition);
+        // console.log(isHold);
+    });
+    sticky.addEventListener("mouseup", (ev) => {
+        isHold = false;
+        sticky.style.cursor = "default";
+    });
+
+    sticky.addEventListener("mousemove", (ev) => {
+        if (isHold) {
+            sticky.style.cursor = "move";
+            stickyPosition[0] = ev.clientX - dx;
+            stickyPosition[1] = ev.clientY - dy;
+            // console.log(stickyPosition);
+            sticky.style.left = `${stickyPosition[0]}px`;
+            sticky.style.top = `${stickyPosition[1]}px`;
+        }
+    });
+
+    return sticky;
 }
 
+
+/**
+ * After translation result is received by background script, create a sticky not to show the result.
+ */
 clientPort.onMessage.addListener((data, sender) => {
-    console.log(data);
-    console.log(sender);
+    // console.log(data);
+    // console.log(sender);
     if (selectedNode) {
-        console.log(selectedNode);
-        console.log(selectedRect);
-        let sticky = createSticky(selectedRect.right, selectedRect.bottom, data.result);
-        if (stickyId) {
-            let lastSticky = document.getElementById(stickyId);
-            if (lastSticky) {
-                lastSticky.remove();
-            }
+        // console.log(selectedNode);
+        // console.log(selectedRect);
+        let contentElement = "";
+        data.result.forEach(i => {
+            contentElement += `<p>${i}</p>`;
+        });
+        // console.log(contentElement);        
+        if (isStickyExists == false) {
+            sticky = createSticky(selectedRect.right, selectedRect.bottom, contentElement);
+            selectedNode.parentNode.appendChild(sticky);
+            isStickyExists = true;
+        } else {
+            let tmpIdArr = sticky.id.split("-");
+            // console.log(tmpIdArr);
+            let stickyContentId = `ext-sticky-notes-${tmpIdArr[tmpIdArr.length - 1]}`;
+            // console.log(stickyContentId);
+            let stickyContent = document.getElementById(stickyContentId);
+            // console.log(stickyContent);
+            stickyContent.innerHTML = contentElement;
         }
-        stickyId = `ext-sticky-${sticky[0]}`;
-        selectedNode.parentNode.appendChild(sticky[1]);
     }
 });
-
 
 document.addEventListener("mouseup",  onSelected);
